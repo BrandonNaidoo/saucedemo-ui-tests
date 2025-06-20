125de8abd62bc9d9477dbe110b0f0edc2ee51eff
BrandonNaidoo <91075787+BrandonNaidoo@users.noreply.github.com>
2025-06-20 12:55:43 +0200

    Chore: Add GitHub pipeline
    Add a GitHub Actions workflow to automate running Cypress UI tests on push, pull request, and scheduled triggers.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import webpackPreprocessor from '@cypress/webpack-preprocessor';
+
+const webpackOptions = require('./webpack.config');
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on) {
+      on('file:preprocessor', webpackPreprocessor({ webpackOptions }));
     },
-    baseUrl: 'https://www.saucedemo.com/',  // 👈 set your baseUrl here
+    baseUrl: 'https://www.saucedemo.com/',
+    supportFile: 'cypress/support/e2e.ts',
     env: {
       users: {
         standard_user: { username: 'standard_user', password: 'secret_sauce' },
         locked_out_user: { username: 'locked_out_user', password: 'secret_sauce' },
         incorrect_username_user: { username: 'incorrect_username_user', password: 'secret_sauce' },
-        incorrect_password_user: { username: 'standard_user', password: 'incorrect_password' }
-      }
-    }
+        incorrect_password_user: { username: 'standard_user', password: 'incorrect_password' },
+      },
+    },
   },
 });
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+// webpack.config.js
+module.exports = {
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
