a3708553c3840ca4b904b09262eaffd337c4fef9
Brandon Naidoo <brandon.naidoo@gmail.com>
2025-06-20 12:25:36 +0200

    Disable support file

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       on('file:preprocessor', webpackPreprocessor({ webpackOptions }));
     },
     baseUrl: 'https://www.saucedemo.com/',
-    supportFile: 'cypress/support/e2e.ts',
+    supportFile: false,
     env: {
       users: {
         standard_user: { username: 'standard_user', password: 'secret_sauce' },
